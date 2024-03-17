e9659ed49c840d59827ce200a874261712543df7
Kwongster <bryant.j.kwong@gmail.com>
2024-03-17 15:15:27 -0700

    first commit from drink mixer

---
+++ client/app/page.js
@@ -0,0 +1,25 @@
+"use client";
+import Title from "@/components/title";
+import Dropdown from "@/components/dropdown";
+import GenerateRecipe from "@/components/GenerateRecipe";
+import RecipeCard from "@/components/RecipeCard";
+import userStore from "@/lib/userStore";
+import { useState } from "react";
+
+export default function Home() {
+  const [loading, setLoading] = useState(false);
+  const { drinkRecipe, drinkImage } = userStore();
+
+  return (
+    <main className="w-full px-12 py-12 md:py-24 space-y-[50px]">
+      <Title />
+      <Dropdown />
+      <div className="flex flex-col items-center">
+        {!drinkRecipe && <GenerateRecipe />}
+        {drinkRecipe && <RecipeCard />}
+      </div>
+      {/* <button onClick={() => console.log(drinkRecipe)}>get data</button>
+      <button onClick={() => console.log(drinkImage)}>get image</button> */}
+    </main>
+  );
+}
