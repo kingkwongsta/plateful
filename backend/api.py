d86555744e1c805f2c4baa0aea36d38cfa9cbf4e
Kwongster <bryant.j.kwong@gmail.com>
2024-03-17 15:40:03 -0700

    Add initial backend files and FastAPI setup

---
+++ backend/api.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI, Query
+from fastapi.middleware.cors import CORSMiddleware
+import os
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/test")
+async def testing():
+    return {"message": "TESTING TESTING TESTING"}
+
+@app.get("/more")
+async def more():
+    return {"1 + 1 = 3"}
+
+from backend.food import generate_food_recipe
+
+@app.get("/food")
+async def get_food(cuisine: str = Query(default=None), course: str = Query(default=None), allergies: str = Query(default=None)):
+    return generate_food_recipe(cuisine, course, allergies)
