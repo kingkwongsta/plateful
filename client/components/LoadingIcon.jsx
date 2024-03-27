1c856d20c0b50a40e9b83482439c121a17fe03e7
Kwongster <bryant.j.kwong@gmail.com>
2024-03-27 10:51:34 -0700

    Add text transition animation to LoadingIcon component

--- client/components/LoadingIcon.jsx
+++ client/components/LoadingIcon.jsx
@@ -1,21 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 
-const LoadingIcon = () => {
+const textArray = [
+  "Gathering the ingredients...",
+  "Measuring just the right amount...",
+  "Preparing with care...",
+  "Finalizing the recipe...",
+];
+
+const TextTransition = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % textArray.length);
+    }, 4000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <div className="flex justify-center items-center">
       <motion.div
-        className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-slate-100"
-        animate={{ rotate: 360 }}
-        transition={{
-          duration: 1,
-          repeat: Infinity,
-          ease: "linear",
-          opacity: { duration: 1 },
-        }}
-      />
+        key={currentIndex} // Key for performance
+        animate={{ opacity: [0, 2, 0] }} // Animate opacity with keyframes
+        transition={{ duration: 2, repeat: Infinity, repeatType: "reverse" }} // Control animation duration and repetition
+      >
+        {textArray[currentIndex]}
+      </motion.div>
     </div>
   );
 };
 
-export default LoadingIcon;
+export default TextTransition;
